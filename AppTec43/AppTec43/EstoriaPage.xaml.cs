e91e282f650ff569d4f8034e5214d1728172cf5c
projetoeai <projeto.eai19@gmail.com>
2019-10-24 22:13:16 -0300

    Add project files.

---
+++ AppTec43/AppTec43/EstoriaPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTec43
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EstoriaPage : ContentPage
+	{
+		public EstoriaPage (string aluno)
+		{
+			InitializeComponent ();
+            switch (aluno)
+            {
+                case "Aluno1":
+                    vNome.Text = "Espiga";
+                    vFoto.Source = "https://i.ibb.co/TtQRmWC/espiga.jpg";
+                    vEstoria.Text = "Jaci";
+                    break;
+                case "Aluno2":
+                    vNome.Text = "Lucas Hardware";
+                    vFoto.Source = "https://i.ibb.co/RDjy8BZ/lucas.jpg";
+                    vEstoria.Text = "Se eu fosse PM";
+                    break;
+                case "Aluno3":
+                    vNome.Text = "Moreti";
+                    vFoto.Source = "https://i.ibb.co/qgCjDqf/moreti.jpg";
+                    vEstoria.Text = "Humor, piada, hahaha";
+                    break;
+                case "Aluno4":
+                    vNome.Text = "Paulo Gibóia";
+                    vFoto.Source = "https://i.ibb.co/PwccYxL/paulo.jpg";
+                    vEstoria.Text = "Manja Rola";
+                    break;
+                case "Aluno5":
+                    vNome.Text = "Pedro";
+                    vFoto.Source = "https://i.ibb.co/xDzMfM9/pedro.jpg";
+                    vEstoria.Text = "Esquerdo, mas não canhoto";
+                    break;
+                case "Aluno6":
+                    vNome.Text = "Rafael Valério";
+                    vFoto.Source = "https://i.ibb.co/1Tqzmjs/valerio.jpg";
+                    vEstoria.Text = "Programador Real";
+                    break;
+                case "Aluno7":
+                    vNome.Text = "Seninha";
+                    vFoto.Source = "https://i.ibb.co/9GvWMsw/seninha.jpg";
+                    vEstoria.Text = "Reencarnação do Ayrton";
+                    break;
+                case "Aluno8":
+                    vNome.Text = "Uzias";
+                    vFoto.Source = "https://i.ibb.co/340HJbw/uzias.jpg";
+                    vEstoria.Text = "Morô, cara";
+                    break;
+                case "Aluno9":
+                    vNome.Text = "Vitor";
+                    vFoto.Source = "https://i.ibb.co/FB8fzz9/vitor.jpg";
+                    vEstoria.Text = "Compra e amarra";
+                    break;
+                case "Aluno10":
+                    vNome.Text = "Yan";
+                    vFoto.Source = "https://i.ibb.co/YDPLNjk/yan.jpg";
+                    vEstoria.Text = "Hacker real";
+                    break;
+                case "Aluno11":
+                    vNome.Text = "Yuri";
+                    vFoto.Source = "https://i.ibb.co/1sjJb71/yuri.jpg";
+                    vEstoria.Text = "Otaku";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private async void BtnNarrar_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync(vEstoria.Text);
+        }
+    }
+}
---
+++ AppTec43/AppTec43/TurmaPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTec43
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TurmaPage : ContentPage
+	{
+        string aluno;
+		public TurmaPage ()
+		{
+			InitializeComponent ();
+		}       
+
+        private void BtnAluno11_Clicked(object sender, EventArgs e)
+        {
+            aluno = "Aluno11";
+            Navigation.PushAsync(new EstoriaPage(aluno));
+        }
+
+        private void BtnAluno10_Clicked(object sender, EventArgs e)
+        {
+            aluno = "Aluno10";
+            Navigation.PushAsync(new EstoriaPage(aluno));
+        }
+
+        private void BtnAluno9_Clicked(object sender, EventArgs e)
+        {
+            aluno = "Aluno9";
+            Navigation.PushAsync(new EstoriaPage(aluno));
+        }
+
+        private void BtnAluno8_Clicked(object sender, EventArgs e)
+        {
+            aluno = "Aluno8";
+            Navigation.PushAsync(new EstoriaPage(aluno));
+        }
+
+        private void BtnAluno7_Clicked(object sender, EventArgs e)
+        {
+            aluno = "Aluno7";
+            Navigation.PushAsync(new EstoriaPage(aluno));
+        }
+
+        private void BtnAluno6_Clicked(object sender, EventArgs e)
+        {
+            aluno = "Aluno6";
+            Navigation.PushAsync(new EstoriaPage(aluno));
+        }
+
+        private void BtnAluno5_Clicked(object sender, EventArgs e)
+        {
+            aluno = "Aluno5";
+            Navigation.PushAsync(new EstoriaPage(aluno));
+        }
+
+        private void BtnAluno4_Clicked(object sender, EventArgs e)
+        {
+            aluno = "Aluno4";
+            Navigation.PushAsync(new EstoriaPage(aluno));
+        }
+
+        private void BtnAluno3_Clicked(object sender, EventArgs e)
+        {
+            aluno = "Aluno3";
+            Navigation.PushAsync(new EstoriaPage(aluno));
+        }
+
+        private void BtnAluno2_Clicked(object sender, EventArgs e)
+        {
+            aluno = "Aluno2";
+            Navigation.PushAsync(new EstoriaPage(aluno));
+        }
+        private void BtnAluno1_Clicked(object sender, EventArgs e)
+        {
+            aluno = "Aluno1";
+            Navigation.PushAsync(new EstoriaPage(aluno));
+        }
+    }
+}
